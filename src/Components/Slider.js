aba5afdd387d047d3210e5b7dda2fded16f77ae5
GilPOP <wolfye@naver.com>
2022-01-17 01:30:47 +0900

    now slider animation start

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -1,28 +1,73 @@
 import { useState } from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const Animation = keyframes`
+0%{
+  opacity: 1;
+  left:0
+}100%{
+  opacity:1;
+  x:20%
+}
+`;
 
 const Wrapper = styled.div`
-  width: 100%;
+  margin: 30px 0px;
+  display: flex;
+  justify-content: center;
+  width: 100vw;
+  overflow-x: hidden;
+  animation: ${Animation} 5s ease-in-out;
 `;
 
 const Container = styled.div`
-  width: 50%;
+  background-color: rgba(0, 0, 0, 1);
+  border-radius: 7px;
+  display: flex;
+  align-items: center;
+  animation: ${Animation} 5s ease-in-out;
 `;
 
 const Row = styled.div`
   display: flex;
-  width: 100%;
-  justify-content: space-between;
+  align-items: center;
+  animation: ${Animation} 5s ease-in-out;
 `;
 
 const Img = styled.img`
-  width: 100%;
+  border-radius: 7px;
+  margin: 0;
+  animation: ${Animation} 5s ease-in-out;
 `;
-const PrivewImg = styled(Img)`
-  background-size: cover;
+const PrivewImg = styled.img`
+  transition: ease-in-out 5s;
+  border-radius: 7px;
+  opacity: 0.5;
 `;
 
-const Button = styled.button``;
+const Button = styled.button`
+  display: flex;
+  align-items: center;
+  position: relative;
+  border: none;
+  font-size: 12px;
+  height: 50px;
+  border-radius: 60px;
+  padding: 25px 10px;
+  opacity: 0.5;
+  z-index: 2;
+  i {
+    color: rgba(0, 0, 0, 0.5);
+  }
+`;
+
+const LeftButton = styled(Button)`
+  right: 50px;
+`;
+
+const RightButton = styled(Button)`
+  left: 50px;
+`;
 
 const WantedImg = [
   "https://static.wanted.co.kr/images/banners/1489/312a0c29.jpg",
@@ -48,11 +93,19 @@ function Slider() {
   return (
     <Wrapper>
       <Row key={index}>
-        <PrivewImg src={WantedImg[PrevImg]}></PrivewImg>
-        <Button onClick={decreaseClick}>Back</Button>
-        <Img src={WantedImg[index]}></Img>
-        <Button onClick={increaseClick}>Next</Button>
-        <PrivewImg src={WantedImg[NextImg]}></PrivewImg>
+        <Container>
+          <PrivewImg id={index - 1} src={WantedImg[PrevImg]}></PrivewImg>
+        </Container>
+        <LeftButton onClick={decreaseClick}>
+          <i class="fas fa-chevron-left"></i>
+        </LeftButton>
+        <Img id={index} src={WantedImg[index]}></Img>
+        <RightButton onClick={increaseClick}>
+          <i class="fas fa-chevron-right"></i>
+        </RightButton>
+        <Container>
+          <PrivewImg id={index + 1} src={WantedImg[NextImg]}></PrivewImg>
+        </Container>
       </Row>
     </Wrapper>
   );
